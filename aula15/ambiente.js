1de5e8ca403df1d0018b13088eacf10bc72d249b
SamuelItalo <samuel.italo.vitorino@gmail.com>
2022-07-29 17:23:51 -0300

    atualizei aula 15

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -3,4 +3,7 @@ console.log(num)
 num.sort()
 console.log(`nosso vetor tem ${num.length} posições`)
 console.log(`o vetor na 1º posição é ${num[0]}`)
-console.log(`e assim ficou nosso vetor ${num}`)
+console.log(`e assim ficou nosso vetor ${num}`) 
+
+ let pos = num.indexOf(12)
+ console.log(`O valor 9 está na posição ${pos}`)
