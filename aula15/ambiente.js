686f2e6dc507242ec1f5b7d5794b0b2d71138c4e
SamuelItalo <samuel.italo.vitorino@gmail.com>
2022-07-29 16:20:25 -0300

    criei aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5,2,8,9,3]
+console.log(num)
+num.sort()
+console.log(`nosso vetor tem ${num.length} posições`)
+console.log(`o vetor na 1º posição é ${num[0]}`)
+console.log(`e assim ficou nosso vetor ${num}`)
